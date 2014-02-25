0091ea4f17a12a218328f04d7d60e50e33922d6d
Scarabele <henriquescarabele@hotmail.com>
2014-02-25 20:30:38 -0300

    add home

--- assets/angularjs/js/controllers.js
+++ assets/angularjs/js/controllers.js
@@ -5,6 +5,14 @@
 angular.module('NcBox.controllers', []).
 	controller('Home', function($scope){
 		$scope.firstname = 'henrique';
+
+		$scope.login = function(){
+			if($scope.loginemail == 'teste@teste.com' && $scope.loginsenha == '123'){
+
+			}else{
+				$scope.errorlogin = 'Login invalido';
+			}
+		}
 	})
 	.controller('MyCtrl2', [function() {
 
