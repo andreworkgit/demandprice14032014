06d52536ef6be9ea053ac0ed216bd8895cd50808
Scarabele <henriquescarabele@hotmail.com>
2014-03-05 13:05:49 -0300

    teste models mongoose

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,40 +1,36 @@
 /**
  * UsersController
  */
-
+var ModelUsers = require('../models/mongoose/Users.js').Users;
 module.exports = {
     
 	_config: {},
-	Users: require('../models/mongoose/Users.js').Users,
-
 	listar: function(req, res, next){
-		//var Users = require('../models/mongoose/Users.js').Users;
-
-		//console.dir(Users);
-
-		//res.json({});
-
-		Users.find({}, function(err, rs){
+		ModelUsers.find({}, function(err, rs){
 			console.dir(rs);
 			res.json(rs);
 		});
-
-
-
-
 	},
-
 	create: function(req, res, next){
-		user = new Users();
+		user = ModelUsers();
 		user.firstname = req.body.firstname;
 		user.lastname = req.body.lastname;
 		user.email = req.body.email;
 		user.password = req.body.password;
 
 		user.save(function(err){
-			console.log("salvo");
-			//res.redirect('/');
-			res.json(user);
+			if(err){
+				res.writeHead(400);
+				req.session.logado = false;
+				req.session.save();
+				res.json(err);
+			}else if(user){
+				req.session.logado = true;
+				req.session.cookie.maxAge = 86400000 * 28;
+				req.session.user = user;
+				req.session.save();
+				res.json(user);
+			}
 		});
 
 
