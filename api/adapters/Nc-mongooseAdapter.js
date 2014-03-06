c79cda2d9c1c354b97feb168814a891cbfd461e5
Andre Augusto <andrework@gmail.com>
2014-03-06 10:20:35 -0300

    test

---
+++ api/adapters/Nc-mongooseAdapter.js
@@ -0,0 +1,78 @@
+
+
+
+module.exports = (function () {
+
+
+  /**
+   * Nc-mongooseAdapter
+   * 
+   * @module      :: Adapter
+   * @description :: A short summary of what this adapter is for and what interfaces it supports.
+   * @docs        :: http://sailsjs.org/#!documentation/adapters
+   *
+   * @syncable    :: false
+   * @schema      :: false
+   */
+
+  var mongoose = require('mongoose'),Modelo;
+
+  var Adapter = {
+
+    /**
+     * registerCollection() is run multiple times (once for each model, aka collection)
+     * before the server ever starts.  It allows us to register our models with the
+     * underlying adapter interface.  (don't forget to cb() when you're done!)
+     */
+
+    registerCollection: function (collection, cb) {
+      console.log('registerCollection');
+      //console.log(collection);
+      //createConnection(collection.config);
+      //createModel(collection.identity);
+
+      cb();
+    },
+
+    define: function(collectionName, definition, cb) {
+      console.log('define');
+      console.log(definition);
+
+    },
+
+    find: function(collectionName,options, cb) {
+       console.log('find');
+       console.log(collectionName);
+       console.log(options);
+       //console.log(par2);
+
+
+       Modelo.find(options, function (err, user) {
+        cb(err,user);
+       });
+
+    },
+
+    identity: 'sails-ncmongoose'
+
+  };
+
+  function createConnection(config) {
+    console.log('createConnection');
+    console.log(config.url);
+    mongoose.connect(config.url);
+
+  }
+
+  function createModel(collectionName){
+
+    var objSchema = require('../models/mongoose/'+collectionName+'.js')(mongoose);
+    //console.log(attrModulo);
+    //var objSchema = new Schema(attrModulo);
+
+    Modelo = mongoose.model(collectionName, objSchema);
+  }
+
+
+	return Adapter;
+})();
---
+++ api/models/mongoose/Users.js
@@ -0,0 +1,30 @@
+
+module.exports = function (config){ 
+	var mongoose = require('mongoose');
+	mongoose.connect(config.url);
+	var Schema = mongoose.Schema;
+	var schemaOptions = {
+	 toJSON: {
+	   virtuals: true 
+	 },
+	 toObject: {
+	   virtuals: true
+	 }
+	};
+
+	var ProjetoSchema = new Schema({
+	 nome  : String,
+	 descricao : String
+	}, schemaOptions);
+
+	var UsersSchema = new Schema({
+	 firstname : String,
+	 lastname : String,
+	 email  : String,
+	 password : String,
+	 projetos  : [ProjetoSchema]
+	});
+
+	return mongoose.model('users', UsersSchema);
+
+};
