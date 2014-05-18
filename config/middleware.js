8bb05d9ec43260f79758020033af13550782bfb5
Andre <andrework@gmail.com>
2014-05-17 22:14:08 -0300

    ts

--- config/middleware.js
+++ config/middleware.js
@@ -148,7 +148,7 @@ module.exports = {
             passport.use(new GoogleStrategy({
                     clientID: '740383275630-4cme4abgmkdcn7surcb35bea9iq9emv0.apps.googleusercontent.com',
                     clientSecret: 'dsYfNik7wJAuGMzJnyiOXkDU',
-                    callbackURL: 'http://localhost:1337/auth/google/callback'
+                    callbackURL: 'http://desventu.herokuapp.com/auth/google/callback'
                 },
                 verifyHandler
             ));
